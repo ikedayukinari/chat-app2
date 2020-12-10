29a76acde9f053ddba41f1c11a1df77dd7800e13
ikedayukinari <nari.1985.0201@gmail.com>
2020-12-10 15:15:23 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
-end
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) #nameカラムを追加したので、このメソッドを使用してnameキーの内容を保存する為にpermitメソッドで許可している
+  end
+end #devise_parameter_sanitizerメソッドは、ユーザー登録をする場合に使うことができ、特定のカラムを許容するメソッド
