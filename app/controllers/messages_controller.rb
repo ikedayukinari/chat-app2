4de1ccff17bdfec4894e77adc19090842e2a4a20
ikedayukinari <nari.1985.0201@gmail.com>
2020-12-11 13:30:00 +0900

    チャットルームの削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new #Messageモデルの空のインスタンスを渡す
     @room = Room.find(params[:room_id])
-    @messages = @room.messages.includes(:user) #チャットルーム紐づいている全てのメッセージ(@room.messages)
+    @messages = @room.messages.includes(:user) #チャットルーム紐づいている全てのメッセージテーブル(@room.messages)
   end #messagesテーブルはルーティングをネストしているためパスが/rooms/:room_id/messagesになっている。パスにroom_idが含まれているため[:room_id]と記述することで取り出せる
 
   def create
@@ -19,7 +19,7 @@ class MessagesController < ApplicationController
 
   private
 
-  def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  def message_params #roomsテーブルとネストを組んでいるのでコントローラーにきている時点でroom_idは埋まっている
+    params.require(:message).permit(:content).merge(user_id: current_user.id)#(room_id: params[:room_id]) @message = @room.new(message_params)の場合
   end
 end
