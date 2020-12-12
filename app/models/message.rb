adc84f79919db42462e29a1059d41b6dd637c1be
ikedayukinari <nari.1985.0201@gmail.com>
2020-12-12 13:51:42 +0900

    画像のみでもメッセージ送信可能となるバリの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,9 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image #Active StoraeのテーブルとMessagesテーブルのアソシエーションを定義,各レコードとファイルを1対1で紐付けるメソッド
   # has_one_attached :ファイル名
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
